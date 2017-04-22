390417d7462822fc836a29fa5f8d9a81f0bf98fa
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2017-04-22 00:25:06 -0400

    Initialize

---
+++ main.py
@@ -0,0 +1,26 @@
+import logging
+import sys
+
+from flask import Flask, url_for, render_template, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+  return render_template('index.html')
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
---
+++ static/js/prototype.js
@@ -0,0 +1,4 @@
+$(function ()
+ {
+   $('#need-js').hide();
+ });
