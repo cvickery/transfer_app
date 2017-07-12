9297947468bee64fdebd047e80d72d6eb241ec40
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2017-07-12 09:12:55 -0400

    Update courses to handle new status field.
    The database now includes both active and inactive courses; display only the
    active ones.

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ def index():
 def assessment():
   return render_template('assessment.html')
 
-# Courses page
+# COURSES PAGE
+# Allows user to pick a college, and see catalog descriptions of all courses offered there.
 @app.route('/courses/', methods=['POST', 'GET'])
 def courses():
   num_courses = 0
@@ -44,14 +45,21 @@ def courses():
     c.execute("select * from designations")
     designations = {row['designation']: row['description'] for row in c}
 
-    c.execute("select name, date_updated from institutions where code ='{}'".format(request.form['inst']))
+    institution_code = request.form['inst']
+    c.execute("select name, date_updated from institutions where code ='{}'".format(institution_code))
     row = c.fetchone()
+    institution_name = row['name']
     date_updated = datetime.datetime.strptime(row['date_updated'], '%Y-%m-%d').strftime('%B %d, %Y')
+    num_active_courses = c.execute("""
+        select count(*) from courses where institution = '{}' and status='A'
+        """.format(institution_code)).fetchone()[0]
 
-    result = "<h1>{} Courses</h1><p class='subtitle'>As of {}</p>".format(row['name'], date_updated)
+    result = """
+      <h1>{} Courses</h1><p class='subtitle'>{:,} active courses as of {}</p>
+      """.format(institution_name, num_active_courses, date_updated)
 
-    query = "select * from courses where institution = '{}' order by discipline, number"\
-      .format(request.form['inst'])
+    query = "select * from courses where institution = '{}' and status = 'A' order by discipline, number"\
+      .format(institution_code)
     c.execute(query)
 
     for row in c:
@@ -73,6 +81,89 @@ def courses():
   # Form not submitted yet or institution has no courses
   if num_courses == 0:
     prompt = '<fieldset><legend>Select a College</legend>'
+    conn = sqlite3.connect('static/db/courses.db')
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    c.execute("select * from institutions order by code")
+    n = 0
+    for row in c:
+      n += 1
+      prompt = prompt + """
+      <div class='institution-select'>
+        <input type="radio" name="inst" id="inst-{}" value="{}">
+        <label for="inst-{}">{}</label>
+      </div>
+      """.format(n, row['code'], n, row['name'])
+    result = """
+    <form method="post" action="">
+      {}
+      <div><button type="submit">Please</button></div>
+    <form>
+    """.format(prompt)
+  return render_template('courses.html', result=Markup(result))
+
+# TRANSFERS PAGE
+# User submits a CUNY email address and lists of sending colleges, external subjects, receiving
+# colleges, and course attributes. Lists may be empty, which means "all." (No email address means
+# "none.")
+# Show a form that can be used to verify the corresponding transfer rules or to note anomalies.
+@app.route('/transfers/', methods=['POST', 'GET'])
+def transfers():
+  num_courses = 0
+  if request.method == 'POST':
+    # User has submitted the form. They must have supplied a cuny email addresss, a list of sending
+    # colleges, a list of external subject areas, and a list of receiving colleges.
+
+    # Get sending college(s)
+    # Get external subject
+    # Get receiving college(s)
+    # Verify the email address. It must match the single receiving college's domain. Otherwise, the
+    # user is in view-only mode.
+    conn = sqlite3.connect('static/db/courses.db')
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+
+    c.execute("select * from external_subjects")
+    cuny_subjects = {row['area']:row['description'] for row in c}
+
+    c.execute("select * from careers")
+    careers = {(row['institution'], row['career']): row['description'] for row in c}
+
+    c.execute("select * from designations")
+    designations = {row['designation']: row['description'] for row in c}
+
+    c.execute("select name, date_updated from institutions where code ='{}'".format(request.form['inst']))
+    row = c.fetchone()
+    institution = row['name']
+    date_updated = datetime.datetime.strptime(row['date_updated'], '%Y-%m-%d').strftime('%B %d, %Y')
+
+    query = "select * from courses where institution = '{}' and status = 'A' order by discipline, number"\
+      .format(request.form['inst'])
+    c.execute(query)
+
+    result = """
+      <h1>{} Courses</h1><p class='subtitle'>{:,} active courses as of {}</p>
+      """.format(institution, len(c), date_updated)
+
+    for row in c:
+      num_courses += 1
+      result = result + """
+      <p class="catalog-entry"><strong>{} {}: {}</strong> (<em>{}; {}</em>)<br/>
+      {:0.1f}hr; {:0.1f}cr; Requisites: <em>{}</em><br/>{} (<em>{}</em>)</p>
+      """.format(row['discipline'],
+                 row['number'].strip(),
+                 row['title'],
+                 careers[(row['institution'],row['career'])],
+                 cuny_subjects[row['cuny_subject']],
+                 float(row['hours']),
+                 float(row['credits']),
+                 row['requisites'],
+                 row['description'],
+                 designations[row['designation']])
+
+  # Form not submitted yet, so generate it.
+  else:
+    prompt = '<fieldset><legend>Select sending college(s)</legend>'
     conn = sqlite3.connect('static/db/courses.db')
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
