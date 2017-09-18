a065cbba52cadbcb6b61061a64731e413e0baf42
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2017-09-18 14:53:09 -0400

    Rewording confirmation email message.

--- sendtoken.py
+++ sendtoken.py
@@ -13,35 +13,33 @@ def send_token(email, url, evaluation_rows='<tr><td>None</td></tr>'):
   to_email = mail.Email(email)
   from_email = mail.Email('Tranfer.Evaluations@provost-access-148820.appspotmail.com')
   subject = 'Link for confirming your evaluations'
-  text_body = mail.Content('text/plain', 'You need to view this message as HTML')
   suffix = 's'
-  if evaluation_rows.count('<tr>') == 1: suffix = ''
+  pronoun = 'them'
+  if evaluation_rows.count('<tr>') == 1:
+    suffix = ''
+    pronoun = 'it'
   html_body = mail.Content('text/html',"""
   <p>
-    Use the button below to confirm that you have evaluated the following transfer rules and
-    are ready to activate them.
-    If you did not perform these evaluations or if you have decided not to have them recorded
-    you may ignore this message.
+    Use the link below to confirm that you have evaluated the following transfer rule{} and
+    are ready to activate {}.<br/>
+    You may ignore this message if you did not perform these evaluations or if you have decided not to have them recorded.
   </p>
   <table style="border-collapse:collapse; border: 1px solid black;">
     {}
   </table>
   <h2>
-    The link below will activate your evaluation{} and open a confirmation page at {}.
+    <a  href="{}"
+        style="font-weight: bold;
+               font-variant:small-caps;
+               padding:0.5em;
+               font-size: 1.3em;"
+               border-radius=0.25em;>{}
+    </a>
   </h2>
-  <a  href="{}"
-      style="font-weight: bold;
-             font-variant:small-caps;
-             padding:0.5em;
-             font-size: 1.3em;"
-             border-radius=0.25em;>
-    activate evaluation{}
-  </a>
+  <p>This link will be expire in 48 hours.</p>
   """.format(evaluation_rows,
-             suffix,
-             parsed_url.netloc.replace('.', '․'),
-             parsed_url.geturl(),
-             suffix))
+             suffix, pronoun,
+             parsed_url.geturl()))
   message = mail.Mail(from_email, subject, to_email, html_body)
   response = sg.client.mail.send.post(request_body=message.get())
   return response
