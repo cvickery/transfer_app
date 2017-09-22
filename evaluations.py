53e6895123cba683a91f304791f9a7b43c0b9241
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2017-09-21 23:23:27 -0400

    Improve UI in confirmation page.
    Change numeric status codes to descriptions; add action and comment columns.

--- evaluations.py
+++ evaluations.py
@@ -7,22 +7,44 @@ from datetime import datetime
 
 from pgconnection import pgconnection
 
+event_type_bits = None
+status_messages = None
+
+def status_string(status):
+  """
+    Generate a string summarizing all bits that are set in status.
+  """
+  if status == 0: return 'Not Evaluated'
+  strings = []
+  bit = 1
+  for i in range(16):
+    if status & bit: strings.append(status_messages[bit])
+    bit += bit
+  return '; '.join(strings)
+
 
 def process_pending(row):
   """ Look up the token and generate events. Return as status message.
   """
+  global event_type_bits, status_messages
+
   token = row['token']
   evaluations = json.loads(row['evaluations'])
   email = row['email']
   when_entered = row['when_entered']
   summaries = ''
-  conn = pgconnection('dbname=cuny_courses')
+  conn = pgconnection('dbname=CUNY_COURSES')
   with conn.cursor() as curr:
 
     event_type_bits = dict()
     curr.execute('select * from event_types')
     for row in curr.fetchall():
       event_type_bits[row['abbr']] = row['bitmask']
+
+    status_messages = dict()
+    curr.execute('select * from transfer_rule_status')
+    for row in curr.fetchall():
+      status_messages[row['value']] = row['description']
 
     for evaluation in evaluations:
 
@@ -60,15 +82,22 @@ def process_pending(row):
       curr.execute(q, (new_status, source_course_id, destination_course_id))
 
       # Generate a summary of this evaluation
+      old_status_str = status_string(old_status)
+      new_status_str = status_string(new_status)
       summaries += """
       <tr>
         <td title="{} => {}">{}</td>
         <td>{}</td>
         <td>{}</td>
+        <td>{}</td>
+        <td>{}</td>
       </tr>
       """.format(evaluation['rule_src_id'],
                  evaluation['rule_dest_id'],
-                 evaluation['rule_str'], old_status, new_status)
+                 evaluation['rule_str'],
+                 evaluation['event_type'],
+                 evaluation['comment_text'],
+                 old_status_str, new_status_str)
     # Remove record from pending_evaluations
     curr.execute('delete from pending_evaluations where token = %s', (token, ))
 
@@ -80,7 +109,7 @@ def process_pending(row):
   <p>Recorded {} evaluation{} made by <em>{}</em> on {}.</p>
     <table>
       <tr>
-      <th>Rule</th><th>Previous Status</th><th>New Status</th>
+      <th>Rule</th><th>Action</th><th>Note</th><th>Previous Status</th><th>New Status</th>
       </tr>
       {}
     </table>
