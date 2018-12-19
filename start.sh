9b608c2434a472436cae10786d0058f44b53518a
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2018-12-19 00:22:02 -0500

    Fix start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,6 @@ gunicorn  --bind 0.0.0.0:5000 \
           --config gunicorn.conf.py \
           --access-logfile ~/Logs/transfer-app-access.log \
           --error-logfile ~/Logs/transfer-app-error.log \
-          main:app
+          --log-level info \
+          main:app &
+echo gunicorn started
