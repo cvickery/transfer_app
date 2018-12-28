86f06681c02b8436907894318249ad560f030ada
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2018-12-28 14:54:09 -0500

    Switch system status system to separate db.
    Memcache didn’t prevent new connections to cuny_courses after existing
    connections were killed, so the update_db script would hang in the middle.

--- system_status.py
+++ system_status.py
@@ -2,13 +2,27 @@
 #   APP_AVAILABLE: True with system is not available. Normally, just means db is being updated, but
 #   could be something more drastic.
 
+from datetime import datetime, timedelta
+
+import psycopg2
+from psycopg2.extras import NamedTupleCursor
+
 from time import perf_counter
 from memcache import Client
 
-client = Client(['127.0.0.1:11211'])
-client.set('APP_AVAILABLE', True)
-client.set('start_update', None)
-client.set('start_maintenance', None)
+APP_AVAILABLE = True
+
+
+# get_status()
+# -------------------------------------------------------------------------------------------------
+def get_status():
+  db = psycopg2.connect('dbname=access_control')
+  cursor = db.cursor(cursor_factory=NamedTupleCursor)
+  cursor.execute('select * from access_control')
+  available = not any([x.start_time for x in cursor.fetchall()])
+  cursor.close()
+  db.commit()
+  return available
 
 
 # app_available()
@@ -16,8 +30,7 @@ client.set('start_maintenance', None)
 def app_available():
   """ Tell whether app is unavailable due to db update or maintenance.
   """
-  global client
-  return client.get('APP_AVAILABLE')
+  return get_status()
 
 
 # app_unavailable()
@@ -25,8 +38,7 @@ def app_available():
 def app_unavailable():
   """ Tell whether app is unavailable due to db update or maintenance.
   """
-  global client
-  return not client.get('APP_AVAILABLE')
+  return not get_status()
 
 
 # get_reason()
@@ -34,30 +46,39 @@ def app_unavailable():
 def get_reason():
   """ Explain app availability.
   """
-  global client
-  if client.get('APP_AVAILABLE'):
-    return 'Application is available.'
-
   return_val = ''
 
-  if client.get('start_update'):
-    time_remaining = perf_counter() - (client.get('start_update') + 1800.0)
-    mins_remaining = abs(int(time_remaining / 60))
-    secs_remaining = abs(time_remaining) - (mins_remaining * 60)
-    if time_remaining < 0.0:
-      when = f'complete within {mins_remaining}:{int(secs_remaining):02} min:sec.'
+  db = psycopg2.connect('dbname=access_control')
+  cursor = db.cursor(cursor_factory=NamedTupleCursor)
+  cursor.execute('select * from access_control')
+  events = dict([(e.event_type, e.start_time) for e in cursor.fetchall()])
+  cursor.close()
+
+  time_now = datetime.now()
+  if events['update_db']:
+    end_update = events['update_db'] + timedelta(seconds=1800)
+    if time_now < end_update:
+      time_remaining = end_update - time_now
+      when = 'complete within {}:{:02} min:sec.'
     else:
-      when = f'have completed {mins_remaining}:{int(secs_remaining):02} min:sec ago.'
+      time_remaining = time_now - end_update
+      when = 'have completed {}:{:02} min:sec ago.'
+    days = time_remaining.days
+    hours, remainder = divmod(time_remaining.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    when = when.format(minutes, seconds)
     return_val = f'<h2>Database update should {when}</h2>'
 
-  if client.get('start_maintenance'):
-    time_elapsed = perf_counter() - client.get('start_maintenance')
-    hours_elapsed = int(time_elapsed / 3600)
-    mins_elapsed = int((time_elapsed - int(hours_elapsed * 3600)) / 60)
-    secs_elapsed = int(time_elapsed - int(hours_elapsed * 3600) - int(mins_elapsed * 60))
+  if events['maintenance']:
+    time_elapsed = time_now - events['maintenance']
+    days = time_elapsed.days
+    hours, remainder = divmod(time_elapsed.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
     return_val += f"""<h2>Maintenance began
-        {hours_elapsed}:{mins_elapsed:02}:{secs_elapsed:02} hr:min:sec ago.</h2>"""
+        {days} days, {hours}:{minutes:02}:{seconds:02} hr:min:sec ago.</h2>"""
 
+  if return_val == '':
+    return_val = 'Application is available.'
   return return_val
 
 
@@ -66,10 +87,12 @@ def get_reason():
 def start_update_db():
   """ Make app unavailable: db update in progress.
   """
-  global client
-  client.set('APP_AVAILABLE', False)
-  client.set('start_update', perf_counter())
-  return client.get('APP_AVAILABLE')
+  db = psycopg2.connect('dbname=access_control')
+  cursor = db.cursor(cursor_factory=NamedTupleCursor)
+  cursor.execute("update access_control set start_time = now() where event_type='update_db'")
+  cursor.close()
+  db.commit()
+  return get_status()
 
 
 # end_update_db()
@@ -77,13 +100,12 @@ def start_update_db():
 def end_update_db():
   """ Make app available when db update ends, unless maintenance in progress.
   """
-  global client
-  client.set('start_update', None)
-  if client.get('start_maintenance'):
-    client.set('APP_AVAILABLE', False)
-  else:
-    client.set('APP_AVAILABLE', True)
-  return client.get('APP_AVAILABLE')
+  db = psycopg2.connect('dbname=access_control')
+  cursor = db.cursor(cursor_factory=NamedTupleCursor)
+  cursor.execute("update access_control set start_time = NULL where event_type = 'update_db'")
+  cursor.close()
+  db.commit()
+  return get_status()
 
 
 # start_maintenance()
@@ -91,10 +113,14 @@ def end_update_db():
 def start_maintenance():
   """ Make app unavailable: unspecified maintenance in progress.
   """
-  global client
-  client.set('APP_AVAILABLE', False)
-  client.set('start_maintenance', perf_counter())
-  return client.get('APP_AVAILABLE')
+  db = psycopg2.connect('dbname=access_control')
+  cursor = db.cursor(cursor_factory=NamedTupleCursor)
+  cursor.execute("""
+                 update access_control set start_time = now() where event_type = 'maintenance'
+                 """)
+  cursor.close()
+  db.commit()
+  return get_status()
 
 
 # end_maintenance()
@@ -102,10 +128,11 @@ def start_maintenance():
 def end_maintenance():
   """ End maintenance mode: app is available unless db update in progress
   """
-  global client
-  client.set('start_maintenance', None)
-  if client.get('start_update'):
-    client.set('APP_AVAILABLE', False)
-  else:
-    client.set('APP_AVAILABLE', True)
-  return client.get('APP_AVAILABLE')
+  db = psycopg2.connect('dbname=access_control')
+  cursor = db.cursor(cursor_factory=NamedTupleCursor)
+  cursor.execute("""
+                 update access_control set start_time = NULL where event_type = 'maintenance'
+                 """)
+  cursor.close()
+  db.commit()
+  return get_status()
