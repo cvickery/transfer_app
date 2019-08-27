8042c333702792cf0584de07b97de040c4bea568
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-08-26 23:30:02 -0400

    Update eslintrc
    Suppress no-invalid-this rule because it's how things are done in jquery.

--- static/js/.eslintrc.js
+++ static/js/.eslintrc.js
@@ -1,29 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    'env': {
+        'browser': true,
+        'es6': true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2015,
-        "sourceType": "module"
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 2015,
+        'sourceType': 'module'
     },
-    "rules": {
-        "indent": [
-            "error",
+    'rules': {
+        'no-invalid-this': 'off',
+        'indent': [
+            'error',
             2
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
+        'linebreak-style': [
+            'error',
+            'unix'
         ],
-        "quotes": [
-            "error",
-            "single"
+        'quotes': [
+            'error',
+            'single'
         ],
-        "semi": [
-            "error",
-            "always"
+        'semi': [
+            'error',
+            'always'
         ]
     }
 };
