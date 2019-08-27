864f86159a063bbf41fbe7514fc0dfb26bab5031
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-08-26 23:32:04 -0400

    Update courses.js
    PEP-8

--- static/js/courses.js
+++ static/js/courses.js
@@ -1,33 +1,43 @@
-$(function() {
+$(function ()
+{
   $('#need-js').hide();
 
   $('button').prop('disabled', true);
 
-  $('input').change(function() {
+  $('input').change(function ()
+  {
     $('button').prop('disabled', false);
   });
 
   // Global action: escape key hides pop-ups; question key shows instructions.
-  $('*').keyup(function(event) {
-    if (event.keyCode === 27) {
+  $('*').keyup(function (event)
+  {
+    if (event.keyCode === 27)
+    {
       $('#pop-up-div').hide();
     }
-    if (event.key === '?') {
+    if (event.key === '?')
+    {
       $('.instructions').show();
     }
   });
 
   // Clicking on the dismiss bar also hides the pop-ups.
-  $('#dismiss-bar').click(function() {
+  $('#dismiss-bar').click(function ()
+  {
     $('#pop-up-div').hide();
   });
 
   // Clicking on the instructions hides them.
-  $('.instructions').click(function() {
-    $(this).hide();
-  });
+  // NOTES: $(this) is invalid (ESLint)
+  //        Replaced hide() (and no-show) with details elements.
+  // $('.instructions').click(function ()
+  // {
+  //   $(this).hide();
+  // });
 
-  $('#show-attributes').click(function(e) {
+  $('#show-attributes').click(function (e)
+  {
     e.stopPropagation();
     $('#pop-up-div')
       .show()
