3207c98f36cab7d681b7016a6d2bf8ff4d6dded4
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-01-07 18:31:41 -0500

    Move test_sendgrid.py to tests and update it.
    Close the PgConnection.

---
+++ tests/test_sendgrid.py
@@ -0,0 +1,82 @@
+#! /usr/bin/env python3
+import os
+from pprint import pprint
+from sendgrid import SendGridAPIClient
+
+from pgconnection import PgConnection
+
+conn = PgConnection()
+cursor = conn.cursor()
+cursor.execute('select * from person_roles')
+people = dict()
+for person in cursor.fetchall():
+  people[person.role] = {'name': person.name, 'email': person.email}
+conn.close()
+
+message = {'personalizations': [{'to': [people['webmaster']],
+                                 # 'cc': [{'email': 'cvickery@gmail.com',
+                                 #        'name': 'Test at Google'},
+                                 #        {'name': 'CC Tester',
+                                 #         'email': 'Christopher.Vickery@qc.cuny.edu'}],
+                                 # 'bcc': [{'email': 'poffice@qc.cuny.edu', 'name': 'QC Provost'}],
+                                 'subject': 'Testing SendGrid'}],
+           'from': {'email': 'cvickery@qc.cuny.edu',
+                    'name': 'CUNY Transfer App'},
+           'content': [{'type': 'text/plain',
+                        'value': 'Don’t worry at all.'},
+                       {'type': 'text/html',
+                        'value': '<h1>It’s Good</h1>'}]
+           }
+
+
+def send_message(message):
+  try:
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+  except Exception as e:
+    print(e)
+
+
+html_value = """
+<html><head>
+<style>
+table {
+  border-collapse: collapse;
+  border: 1px solid blue;
+}
+td, th {
+  border: 1px solid red;
+  padding: 0.25em;
+}
+</style>
+  </head>
+  <body>
+<p>The following transfer rule review has been submitted by <em>cvickery@qc.cuny.edu</em> on July 03, 2019 at 06:17 PM.</p>
+    <table>
+      <tr>
+        <th>Rule</th>
+        <th>Previous Review Status</th>
+        <th>New Review Status<br/><em>Click for Review History</em></th>
+      </tr>
+
+    <tr>
+      <td><table><tr class="rule "><td title="Queensborough">QCC</td><td>Pass <span title="course_id=71">ARTH 115</span></td><td>3.0 =&gt; 3.0</td><td title="Queens">QNS</td><td><span title="Art History">ARTH</span>-<span title="course id: 3974">113</span></td></tr></table></td>
+      <td>Sender Approved</td>
+      <td>
+    <a href="http://localhost:5000/history/QCC01-QNS01-AR-13" target="_blank">Sender Approved</a></td>
+    </tr>
+    </table></body>
+"""
+
+if __name__ == '__main__':
+  # message['personalizations'][0]['cc'] = [{'email': 'cvickery@gmail.com',
+  #                                          'name': 'Test at Google'},
+  #                                         {'name': 'Test at QC',
+  #                                          'email': 'christopher.vickery@qc.cuny.edu'}]
+  # message['personalizations'][0]['bcc'] = [{'email': 'poffice@qc.cuny.edu', 'name': 'QC Provost'}]
+  # message['content'][1] = {'type': 'text/html', 'value': html_value}
+  pprint(message)
+  send_message(message)
