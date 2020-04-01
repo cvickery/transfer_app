47640d014a10339648e5e57685633e0d9c1bc212
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-04-01 00:10:41 -0400

    Update main.py & requirements.{css|js}
    Add line numbers to scribe blocks. Click to toggle on/off.

--- static/js/requirements.js
+++ static/js/requirements.js
@@ -1,7 +1,6 @@
 /*  A "closeable" is an element for which the contents (first child) can be opened or closed.
  */
 
-
 window.addEventListener('load', function()
 {
   const closers = document.getElementsByClassName('closer');
@@ -25,4 +24,24 @@ window.addEventListener('load', function()
       }
     });
   }
+
+  //  Clicking in a Scribe Block's text toggles line numbers
+  const code_sections = document.getElementsByClassName('with-numbers');
+  for (let i = 0; i < code_sections.length; i++)
+  {
+    code_sections[i].addEventListener('click', function()
+    {
+      const pre_element = this.getElementsByClassName('line-numbers')[0];
+      pre_element.classList.toggle('open');
+      if (pre_element.classList.contains('open'))
+      {
+        pre_element.style.display = 'block';
+      }
+      else
+      {
+        pre_element.style.display = 'none';
+      }
+    });
+  }
+
 });
