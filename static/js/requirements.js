bee2e00d4f590c4daf52a6fc9642b814da80ade5
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-11-13 10:42:20 -0500

    Update requirements.js
    Remove dead code for toggling line number.
    Unsuccessful attempt to add support for toggling state of details
    elements when a key is pressed in them.

--- static/js/requirements.js
+++ static/js/requirements.js
@@ -3,8 +3,22 @@
 
 window.addEventListener('load', function()
 {
-  const closers = document.getElementsByClassName('closer');
 
+  // Typing ESC while hovering over a closer toggles between open and closed.
+  const details = document.getElementsByTagName('details');
+  for (let i = 0; i < details.length; i++)
+  {
+    // This is only getting keyup events when the focus is on the summary, not the body of the
+    // details elements, which defeats the purpose of wanting to close (toggle) the state by
+    // typing escape while hovering over the body part. Sigh.
+    details[i].addEventListener('keyup', function(e)
+    {
+      console.log(e);
+    });
+  }
+
+
+  const closers = document.getElementsByClassName('closer');
   for (let i = 0; i < closers.length; i++)
   {
     // Add the closeable class to the first child
@@ -25,23 +39,23 @@ window.addEventListener('load', function()
     });
   }
 
-  //  Clicking in a Scribe Block's text toggles line numbers
-  const code_sections = document.getElementsByClassName('with-numbers');
-  for (let i = 0; i < code_sections.length; i++)
-  {
-    code_sections[i].addEventListener('click', function()
-    {
-      const pre_element = this.getElementsByClassName('line-numbers')[0];
-      pre_element.classList.toggle('open');
-      if (pre_element.classList.contains('open'))
-      {
-        pre_element.style.display = 'block';
-      }
-      else
-      {
-        pre_element.style.display = 'none';
-      }
-    });
-  }
+  // //  Clicking in a Scribe Block's text toggles line numbers
+  // const code_sections = document.getElementsByClassName('with-numbers');
+  // for (let i = 0; i < code_sections.length; i++)
+  // {
+  //   code_sections[i].addEventListener('click', function()
+  //   {
+  //     const pre_element = this.getElementsByClassName('line-numbers')[0];
+  //     pre_element.classList.toggle('open');
+  //     if (pre_element.classList.contains('open'))
+  //     {
+  //       pre_element.style.display = 'block';
+  //     }
+  //     else
+  //     {
+  //       pre_element.style.display = 'none';
+  //     }
+  //   });
+  // }
 
 });
